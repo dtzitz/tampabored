99d3db57b6abf493cc84103f240d5c480e3f8630
Dylan <dtzitz@live.com>
2017-06-27 08:04:24 -0400

    adds events app and updates django

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -4,12 +4,14 @@ from django.contrib import admin
 admin.autodiscover()
 
 import hello.views
+import events.views
 
 # Examples:
 # url(r'^$', 'gettingstarted.views.home', name='home'),
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
+    url(r'^events/', include('events.urls')),
     url(r'^$', hello.views.index, name='index'),
     url(r'^db', hello.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
