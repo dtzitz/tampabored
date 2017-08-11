e22f5fb0d63718ade521c9f793ffae63b5bd8227
Dylan <dtzitz@live.com>
2017-08-10 20:57:29 -0400

    adds summary to model and details

--- events/admin.py
+++ events/admin.py
@@ -5,7 +5,7 @@ from .models import Event
 # Register your models here.
 
 class EventAdmin(admin.ModelAdmin):
-    fields = ['title', 'start_date', 'end_date', 'category' ]
+    fields = ['title', 'summary', 'start_date', 'end_date', 'category' ]
     list_display = ('title','start_date', 'end_date', 'category')
 
 admin.site.register(Event, EventAdmin)
--- events/models.py
+++ events/models.py
@@ -4,7 +4,8 @@ from django.db import models
 # Create your models here.
 
 class Event(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
+    summary = models.TextField(default='Hodor') 
     start_date = models.DateTimeField('start date')
     end_date = models.DateTimeField('end date')
     category = models.CharField(
