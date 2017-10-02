918b08e37ec75b52debae1eb5380cb05ebc7663d
Dylan <dtzitz@live.com>
2017-10-02 17:44:49 -0400

    adds recurring events page

--- events/views.py
+++ events/views.py
@@ -27,8 +27,8 @@ class DetailView(generic.DetailView):
 def contact(request):
     return render(request, 'contact.html')
 
-def all(request):
-    now = datetime.now(pytz.timezone('US/Eastern'))
-    elist = Event.objects.filter(end_date__gte=now).order_by('start_date')
-    context = {'event_list': elist}
-    return render(request,'elist.html', context)
+def recurring(request):
+    # now = datetime.now(pytz.timezone('US/Eastern'))
+    # elist = Event.objects.filter(end_date__gte=now).order_by('start_date')
+    # context = {'event_list': elist}
+    return render(request,'recurring.html')
--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     url(r'^events/', include('events.urls')),
     url(r'^$', events.views.index, name='index'),
     url(r'^contact/', events.views.contact, name='contact'),
+    url(r'^recurring/', events.views.recurring, name='recurring'),
     # url(r'^$', hello.views.index, name='index'),
     url(r'^db', hello.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
